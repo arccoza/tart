79e067d3163add6265b35b455524f4b52ba98eb2
Adrien <adrien@arccoza.com>
2018-08-21 16:53:17 +0900

    Make labels optional

--- src/js/selfie.js
+++ src/js/selfie.js
@@ -1,3 +1,13 @@
-const selfie = (that, labels) => labels.reduce((that, l) => that[l] = that[l].bind(that, that), that)
+const selfie = (that, labels=null) => {
+  if (!labels) {
+    labels = Object.getOwnPropertyNames(that.constructor.prototype)
+      .filter(k => k !== 'constructor' && typeof(that[k]) === 'function')
+  }
+  if (!labels.length)
+    return
+  console.log(labels)
+
+  return labels.reduce((that, l) => that[l] = that[l].bind(that, that), that)
+}
 
 export {selfie}
