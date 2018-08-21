423a9be823520f9da26c7b6150f4daef2681b246
Adrien <adrien@arccoza.com>
2018-08-21 17:14:14 +0900

    Swap from using bind to a wrapper fn to support arrow and bound fns

--- src/js/selfie.js
+++ src/js/selfie.js
@@ -1,13 +1,16 @@
 const selfie = (that, labels=null) => {
   if (!labels) {
     labels = Object.getOwnPropertyNames(that.constructor.prototype)
-      .filter(k => k !== 'constructor' && typeof(that[k]) === 'function' && that[k].hasOwnProperty('prototype'))
+      .filter(k => k !== 'constructor' && typeof(that[k]) === 'function')
   }
   if (!labels.length)
     return
   // console.log(labels)
 
-  return labels.reduce((that, l) => that[l] = that[l].bind(that, that), that)
+  return labels.reduce((that, l) => {
+    var fn = that[l]
+    return that[l] = function(...args) { return fn.call(that, that, ...args) }
+  }, that)
 }
 
 export {selfie}
