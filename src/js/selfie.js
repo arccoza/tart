1d680bb5aca989954e3dd1563f2d55b2e2161c90
Adrien <adrien@arccoza.com>
2018-08-21 16:56:19 +0900

    Check for previously bound fns and arrow fns

--- src/js/selfie.js
+++ src/js/selfie.js
@@ -1,11 +1,11 @@
 const selfie = (that, labels=null) => {
   if (!labels) {
     labels = Object.getOwnPropertyNames(that.constructor.prototype)
-      .filter(k => k !== 'constructor' && typeof(that[k]) === 'function')
+      .filter(k => k !== 'constructor' && typeof(that[k]) === 'function' && func.hasOwnProperty('prototype'))
   }
   if (!labels.length)
     return
-  console.log(labels)
+  // console.log(labels)
 
   return labels.reduce((that, l) => that[l] = that[l].bind(that, that), that)
 }
