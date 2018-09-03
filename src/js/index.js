26b6520fe64c5728466905f56023c511103efd7f
Adrien <adrien@arccoza.com>
2018-09-03 15:46:06 +0900

    Add Freezer js, use freezer store with streams

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,38 @@
 import {el, list, mount} from 'redom'
 import flyd from './flyd.js'
+import ffilter from 'flyd/module/filter'
 import colours from './colour_data.json'
 import {Palette, Terminal} from './components.js'
+import Freezer from 'freezer-js'
 
+
+const fromFreezer = (fz, ev) => {
+  const s = flyd.stream()
+  fz.on(ev, (a, b) => s([a, b]))
+  return s
+}
+
+// Data
+const ds = new Freezer({
+  lineData: [],
+  pillData: [],
+  colourData: [],
+})
 
 //Data streams
-var lineData = flyd.stream([{text:'a', style:null}, {text:'b', style:null}, {text:'c', style:null}])
-var pillData = flyd.stream([{text:'Foreground', active:true}, {text:'Background'}, {text:'Wha?'}]).map(v => (v._tag = 'FGBG', v))
-var colourData = flyd.stream(colours)
+var lineData = fromFreezer(ds, 'update').map(([a, b]) => a.lineData == b.lineData ? undefined : a.lineData)
+  .pipe(ffilter(v => v !== undefined))
+var pillData = fromFreezer(ds, 'update').map(([a, b]) => a.pillData == b.pillData ? undefined : a.pillData)
+  .pipe(ffilter(v => v !== undefined))
+var colourData = fromFreezer(ds, 'update').map(([a, b]) => a.colourData == b.colourData ? undefined : a.colourData)
+  .pipe(ffilter(v => v !== undefined))
+
+
+var state = ds.get()
+state = state.set('lineData', [{text:'a', style:null}, {text:'b', style:null}, {text:'c', style:null}])
+state = state.set('pillData', [{text:'Foreground', active:true}, {text:'Background'}, {text:'Wha?'}])
+state = state.set('colourData', colours)
+
 
 
 const head = el('header.h', 'tart')
@@ -18,14 +43,23 @@ const main = el('div.main', [pal, head, term, foot])
 
 // update with data
 
+flyd.on(v => console.log(v), pillData)
+// flyd.immediate(
+//   flyd.combine((...args) => {
+//     var [l, p, c, me, ch] = args
+//     console.log(l(), p(), c())
+//   }, [lineData, pillData, colourData])
+// )
+
+
 // lineData.redom = term, pillData.redom = pal, colourData.redom = pal
 flyd.on(v => console.log(v), pal.pills.action)
-lineData.map(v => term.update({lineData}))
-pillData.map(v => pal.update({hidden:false, pillData:v}))
-// pal.update({hidden:false, pillData})
-colourData.map(v => pal.update({colourData}))
+// lineData.map(v => term.update({lineData}))
+// pillData.map(v => pal.update({hidden:false, pillData:v}))
+// // pal.update({hidden:false, pillData})
+// colourData.map(v => pal.update({colourData}))
 
-flyd.on(act => pillData(act.pch(act, pillData())), pal.pills.action)
+// flyd.on(act => pillData(act.pch(act, pillData())), pal.pills.action)
 
 // flyd.combine((lineData, pillData, colourData, me, ch) => {
 //   console.log(ch.map(s => s.redom))
@@ -43,8 +77,11 @@ mount(document.body, main)
 // schedule another update
 setTimeout(() => {
   // term.update({lineData: [{text:'e', style:null}, {text:'f', style:null}, {text:'g ', style:null}]})
-  lineData([{text:'e', style:null}, {text:'f', style:null}, {text:'g ', style:null}])
+  // lineData([{text:'e', style:null}, {text:'f', style:null}, {text:'g ', style:null}])
   // pal.update({hidden: false, pillData: pillData([...pillData(), {text:'Wha2?'}]), colourData})
   // pillData([...pillData(), {text:'Wha2?'}])
-  pillData([...pillData().filter((v, i) => i != 1)])
+  // pillData([...pillData().filter((v, i) => i != 1)])
+
+  state = state.set('pillData', [{text:'Foreground', active:true}, {text:'Background'}, {text:'Wha?'}])
+
 }, 5000)
